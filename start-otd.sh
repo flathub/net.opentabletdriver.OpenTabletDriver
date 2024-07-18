1e7dd5544157ae040ee0f36ab2e507c0890d60b8
Eugenij Safonov <ja.safonau@protonmail.com>
2024-07-18 20:47:23 +0200

    Add launch argument to disable GUI start
    update startup script

--- start-otd.sh
+++ start-otd.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Check arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -d|--daemon-only)
+      DAEMON_ONLY=1
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      echo "Usage: -d|--daemon-only    Do not start configuration utility, only the driver"
+      exit 1
+      ;;
+  esac
+done
+
 # Check if OpenTabletDriver.Daemon is running
 if ! ps ax | grep -v grep | grep "OpenTabletDriver.Daemon" > /dev/null
 then
@@ -11,6 +27,10 @@ else
 fi
 
 # Start OpenTabletDriver.UX.Gtk with exec, replacing the current shell
-echo "Starting OpenTabletDriver.UX.Gtk with exec..."
-exec /app/bin/OpenTabletDriver.UX.Gtk
-
+if [[ $DAEMON_ONLY != 1 ]]
+then
+    echo "Starting OpenTabletDriver.UX.Gtk with exec..."
+    exec /app/bin/OpenTabletDriver.UX.Gtk
+else
+    echo "Skipping configuration utility"
+fi
